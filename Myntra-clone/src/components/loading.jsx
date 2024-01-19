f9dbd154d7138dc9114e896bc616f0cfd6239075
Hariomkumar2023 <qwerthkm123@gmail.com>
2024-01-19 12:08:42 +0530

    committ

---
+++ Myntra-clone/src/components/loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <center className="loading">
+      <div className="spinner-border text-primary" role="status"></div>
+    </center>
+  );
+};
+export default Loading;
---
+++ Myntra-clone/src/routes/Bag.jsx
@@ -0,0 +1,28 @@
+import Bagsummary from "../components/Bagsummary";
+import BagItem from "../components/BagItem";
+import { useSelector } from "react-redux";
+
+const Bag = () => {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id);
+    return itemIndex >= 0;
+  });
+  return (
+    <>
+      <main>
+        <div className="bag-page">
+          <div className="bag-items-container">
+            {finalItems.map((item) => (
+              <BagItem item={item} />
+            ))}
+          </div>
+          <Bagsummary />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Bag;
